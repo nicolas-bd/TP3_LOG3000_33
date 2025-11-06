4837d29fa2e97d2c9cb9ef02ac566a971add8748
Mahdi Gargouri <mahdi.gargouri@polymtl.ca>
2025-11-06 16:00:03 -0500

    doc: documenatation des ficheiers app et operators

--- operators.py
+++ operators.py
@@ -1,11 +1,20 @@
-def add(a,b):
+"""
+Module contenant les fonctions d’opérations arithmétiques utilisées par la calculatrice.
+Chaque fonction prend deux valeurs et retourne le résultat correspondant à l’opération.
+"""
+
+def add(a, b):
+    """Retourne la somme de deux nombres."""
     return a + b
 
-def subtract(a,b):
+def subtract(a, b):
+    """Retourne le résultat de la soustraction entre deux nombres."""
     return b - a
 
-def multiply(a,b):
+def multiply(a, b):
+    """Retourne le résultat de la multiplication entre deux nombres."""
     return a ** b
 
-def divide(a,b):
+def divide(a, b):
+    """Retourne le résultat de la division entre deux nombres."""
     return a // b
