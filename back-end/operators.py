e65382b81df4aba540117ee6438a467d0cb78ce3
William Faraoni <wilfaraoni@gmail.com>
2026-02-17 10:47:03 -0500

    Ajout des docstring aux fichiers de code

--- back-end/operators.py
+++ back-end/operators.py
@@ -1,11 +1,54 @@
+
+
+
 def add(a,b):
+    """Fonction qui additionne un nombre à un autre ( a + b )
+
+    Parameters:
+    a (Any): Premier nombre
+    b (Any): Nombre à additionner
+
+    Returns:
+    Any: Somme des deux nombres
+
+   """
     return a + b
 
 def subtract(a,b):
+    """Fonction qui soustrait un nombre à un autre ( a - b )
+
+    Parameters:
+    a (Any): Premier nombre
+    b (Any): Nombre à soustraire
+
+    Returns:
+    Any: Différence des deux nombres
+
+   """
     return b - a
 
 def multiply(a,b):
+    """Fonction qui multiplie un nombre à un autre ( a * b )
+
+    Parameters:
+    a (Any): Premier nombre
+    b (Any): Nombre à multiplier
+
+    Returns:
+    Any: Produit des deux nombres
+
+   """
     return a ** b
 
 def divide(a,b):
+    """Fonction qui divise un nombre à un autre ( a / b )
+
+    Parameters:
+    a (Any): Premier nombre
+    b (Any): Nombre à diviser
+
+    Returns:
+    Any: Quotient des deux nombres
+
+   """
     return a // b
